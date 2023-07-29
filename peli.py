e69309a9c70de689f844c47f86053c5e69fae29b
MisoMikke <mikaelhukkuulumeen@gmail.com>
2023-07-29 11:15:14 +0300

    parempi

--- peli.py
+++ peli.py
@@ -1,5 +1,37 @@
 import random
 with open('neverever.txt', 'r') as file:
     kysymys = file.readlines()
-valinta = random.choice(kysymys)
-print("never have I ever " +  valinta)
+print("Tervetuloa pelaamaan never have I ever -peliä!")
+print("Aloittaaksesi pelin, kirjoita 'aloita'. Lukeaksesi pelin säännöt, kirjoita 'säännöt'.")
+aloita = input("Kirjoita 'lopeta' päättääksesi ohjelman. ")
+
+def säännöt():
+    print("Never have I ever pelissä...")
+    global jatka
+    jatka = input("Haluaisitko pelata? kirjoita 'aloita' tai 'lopeta'. ")
+    if jatka == "aloita":
+        pelaa()
+    if jatka == "lopeta":
+        loppu()
+    
+def pelaa():
+    valinta = random.choice(kysymys)
+    print("never have I ever " + valinta)
+    uusi = input("uudestaan? y, n? ")
+    if uusi == "y":
+        pelaa()
+    elif uusi == "n":
+        loppu()
+
+def loppu():
+    print("Kiitos ja näkemiin!")
+
+if aloita == "aloita":
+    pelaa()
+elif aloita == "säännöt":
+    säännöt()
+elif aloita == "lopeta":
+    loppu()
+#looppi
+#valinta = random.choice(kysymys)
+#print("never have I ever " +  valinta)
